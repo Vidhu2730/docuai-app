1f1d9734fe56abebe44e8728ef09076a4a93e166
Vidhu <v.tripathi@elsevier.com>
2024-11-29 10:06:12 +0530

    Fixing the padding issues

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
 
   return (
     <div className="App">
-      <div style={{ position: "relative", height: "800px", width: "700px" }}>
+      <div style={{ position: "relative", height: "800px", width: "750px" }}>
         <MainContainer>
           <ChatContainer>
             <MessageList 
